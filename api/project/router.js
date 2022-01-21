e7d64284a8edab97f7e23c29a9c5274291b307ac
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-21 13:07:41 -0800

    spent way too long on this boolean problem so get isnt working

--- api/project/router.js
+++ api/project/router.js
@@ -2,4 +2,17 @@
 const router = require('express').Router()
 const Project = require('./model')
 
+router.get('/', async (req, res, next) => {
+    try {
+        const projects = await Project.getAll()
+            if (projects.project_completed === 0) {
+                    res.json(projects).stringify({project_completed: false})
+            } else {
+                res.json(projects).stringify({project_completed: true})
+            }
+    } catch (err) {
+        next(err)
+    }   
+})
+
 module.exports = router
