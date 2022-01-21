688e566bce9bf089e317651c9aaf7fb74b558702
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-21 15:13:46 -0800

    can get task with project info but boolean still not figured out

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,14 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+async function getAll() {
+    const rows = await db('tasks as t')
+        .leftJoin('projects as p', 't.project_id', 'p.project_id')
+        .groupBy('t.task_id')
+        .select("t.*", "project_name", "project_description")
+    return rows
+}
+
+module.exports = {
+    getAll
+}
--- api/task/router.js
+++ api/task/router.js
@@ -2,4 +2,12 @@
 const router = require('express').Router()
 const Task = require('./model')
 
+router.get('/', (req, res, next) => {
+    Task.getAll()
+        .then(tasks => {
+            res.json(tasks)
+        })
+        .catch(next)
+})
+
 module.exports = router
